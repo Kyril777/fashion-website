15673733e17cfdd4ead6d3a8bb6442d060da5d7f
Kyril777 <Kyril.Jay@gmail.com>
2016-12-01 11:41:18 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Bucky's Template</title>
+	
+	<!-- CSS Normalize. -->
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	
+	<!-- CSS. -->
+	<link rel="stylesheet" type="text/css" href="css/style_large.css">
+	
+	<!-- CSS animate. -->
+	<link rel="stylesheet" type="text/css" href="css/animate.min.css">
+	
+	<!-- Style new HTML5 elements. -->
+	<!--[if lt IE 9]>
+		<script src="js/html5-shiv.js"><.script>
+	<![endif] -->
+	
+</head>
+
+<body>
+	<div id="page">
+		<div id="wrapper">
+		
+			<!-- Header. -->
+			<div id="header">
+				<p class="announcement">Announcement: Stay tuned for our next Fashion SummerStorm!	</p>
+			</div><!-- End of header. -->
+			
+			<!-- Aside. -->
+			<aside>
+				
+				<!-- Logo. -->
+				<div id="logoContainer"><a href="index.php"><img src="img/logo.png" id="logo" alt="New Think Tank Logo"></a></div><!-- Image source: <a href="http://www.freepik.com/free-vector/pink-and-purple-logo-with-two-hearts_848940.htm">Designed by Freepik</a> -->
+
+					<!-- Navigation menu. -->
+					<nav>
+						<span class="nav-btn"></span>
+						<ul class="nav sb-icon-list">
+							<li class="sb-item" id="home">
+								<a href="index"><span class="list-span"><p id="home" class="menuItems">Home</p></span></a>
+							</li>
+							<li class="sb-item">
+								<a href="about"><span class="list-span"><img src="img/about-icon.png" id="first-sb"><p id="about" class="menuItems">About</p></span></a>
+							</li>
+							<li class="sb-item">
+								<a href="contactus"><span class="list-span"><img src="img/contactus-icon.png" id="second-sb"><p id="contactus" class="menuItems">Contact</p></span></a>
+							</li>
+							<li class="sb-item">
+								<a href="gallery"><span class="list-span"><img src="img/gallery-icon.png" id="third-sb"><p id="gallery" class="menuItems">Gallery</p></span></a>
+							</li>
+							<li class="sb-item">
+								<a href="press"><span class="list-span"><img src="img/about-icon.png" id="fourth-sb"><p id="press" class="menuItems">Press</p></span></a>
+							</li>
+						</ul><!-- End of nav sb-icon-list. -->
+					</nav><!-- End of navigation menu. -->
+					
+					<!-- Social media icons. -->
+					<div id="social">
+						<span id="social-title">LET'S TALK</span><br />
+						<a href="#" id="facebook-icon" title="facebook" class="tooltip"><img class="social-icons" src="img/facebook-icon.png"></a>
+						<a href="#" id="twitter-icon" title="twitter" class="tooltip"><img class="social-icons" src="img/twitter-icon.png"></a>
+						<a href="#" id="rss-icon" title="rss" class="tooltip"><img class="social-icons" src="img/rss-icon.png"></a>
+					</div><!-- End of social. -->
+				
+			</aside><!-- End of aside. -->
+			
+			<!-- Files from the contents folder: index.php, about.php (with face.php), contactus.php (with processing_data.php), gallery.php, and press.php. -->
+			<div id="content"></div>
+			
+			<!-- Footer. -->
+			<div id="footer">
+				<div class="footer-text" style="color:#C0C0C0;">
+					<b>Links</b><br/>
+					<a href="#">Models R'Us</a><br/>
+					<a href="#">Glam is Good</a><br/>
+					<a href="#">Golden Accessories</a><br/>
+					<a href="#">Photo Magazine</a><br/>	
+				</div><!-- End of footer-text. -->
+				<div class="footer-text" style="color:#C0C0C0;">
+					<b>Two Hearts Agency</b><br/>
+					3384 Forest Avenue<br>
+					Bayonne, NJ 07002<br/>
+					(201)555-1212<br/>
+					<a href="mailto:#">info@twoheartsagency.com</a>
+				</div><!-- End of footer-text. -->
+			</div><!-- End of footer. -->
+		</div><!-- End of wrapper. -->
+	</div><!-- End of page. -->
+
+	<!-- jQuery. -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	
+	<!-- Javascript. -->
+	<script src="js/javascript.js"></script>
+
+</body>
+</html>
